e1c77a13ca5a26e25f51c811440bb9339567dee7
ym-liu <ymliuy@gmail.com>
2024-09-30 16:55:25 -0400

    fix: enforce correct command syntax in argument parser

--- A1/dnsClient.py
+++ A1/dnsClient.py
@@ -2,19 +2,33 @@ import argparse
 import socket
 
 if __name__ == "__main__":
-    # get the command line arguments (stdin)
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--timeout", "-t", default=5)
-    parser.add_argument("--max_retries", "-r", default=3)
-    parser.add_argument("--port", "-p", default=53)
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("--mail_server", "-mx", action="store_true", default=False)
-    group.add_argument("--name_server", "-ns", action="store_true", default=False)
 
+    """parse the command line arguments (stdin)"""
+    # create a parser
+    parser = argparse.ArgumentParser(
+        allow_abbrev=False  # TODO: allow_abbrev=False doesn't work for some reason
+    )
+
+    # optional arguments
+    parser.add_argument("-t", type=int, default=5, dest="timeout")
+    parser.add_argument("-r", type=int, default=3, dest="max-retries")
+    parser.add_argument("-p", type=int, default=53, dest="port")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-mx", action="store_true", default=False)
+    group.add_argument("-ns", action="store_true", default=False)
+
+    # required, positional arguments
+    parser.add_argument("server", type=str)
+    parser.add_argument("name", type=str)
+
+    # parse the arguments with the previously defined parser
     args = parser.parse_args()
 
-    # send query to server for given domain name using UDP socket
+    """send query to server for given domain name using UDP socket"""
+    # TODO: [INSERT OUTLINE]
 
-    # wait for response to be returned from server
+    """wait for response to be returned from server"""
+    # TODO: [INSERT OUTLINE]
 
-    # interpret response and output result to terminal display (stdout)
+    """interpret response and output result to terminal display (stdout)"""
+    # TODO: [INSERT OUTLINE]
